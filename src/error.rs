bb9a63435cb8225901d37bb45b6fa5c63ba78a11
Michael Mitteregger <michael.mitteregger@gmx.at>
2020-06-12 21:34:14 +0200

    Add `Send` and `Sync` trait bounds to `Error` type

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 /// Simple opaque error type for this library.
 ///
 /// The real cause can be downcasted if necessary.
-pub type Error = Box<dyn std::error::Error>;
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
 
 /// Type alias for `Result<T, mpv_socket::Error>`.
 pub type Result<T> = std::result::Result<T, Error>;
